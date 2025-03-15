ec4f9bff21b0e1e92877cb04aed467b3e799f3c5
Rain <sigrain@outlook.jp>
2025-03-16 08:58:00 +1100

    Implement uploader and voice detection based editing processer

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, send_from_directory, send_file
 from flask_cors import CORS
 from flask_socketio import SocketIO
 from routes import initialize_routes
+from create_video import create_video
 from services.service_audio import audio_service
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -11,9 +13,39 @@ socketio = SocketIO(app, cors_allowed_origins="*", async_mode="threading")
 
 @app.route('/api/edit_video')
 def edit_video():
+  create_video()
   return jsonify({"response": "success"})
 
-# Initialize all routes and services
+@app.route('/api/upload', methods=["POST"])
+def upload_video():
+  file = request.files['file']
+  file_name = request.form.get('file_name')
+
+  if file:
+    try:
+      file_path = "./backend/" + file_name
+      file.save(file_path)
+      return jsonify({'message': f'File center_video successfully uploaded to database'}), 200
+    except Exception as e:
+        return jsonify({'error': f'Failed to save file: {str(e)}'}), 500
+    
+@app.route('/api/download', methods=["GET"])
+def download_video():
+  video_path = "./backend/output.mp4"
+
+  if not os.path.exists(video_path):
+        return jsonify({"error": "Video file not found"}), 404
+  
+  # video = send_from_directory("backend", "output.mp4", mimetype="video/mp4", as_attachment=False)
+  video = send_file("output.mp4", mimetype="video/mp4", as_attachment=False)
+  print('video', video)
+  return video
+
+@app.route('/api/test', methods=["GET"])
+def api_test():
+  return jsonify({"message": "success"})
+
+# Initialize all routes
 initialize_routes(app, socketio)
 audio_service(socketio)
 
